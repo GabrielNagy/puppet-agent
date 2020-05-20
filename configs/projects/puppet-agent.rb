96d087b6513eb3336489eec90059e5177382abee
Ciprian Badescu <ciprian.badescu@puppet.com>
2020-05-20 15:01:14 +0300

    (fix) metadata changes are not working with vanagon-built-runtime

--- configs/projects/puppet-agent.rb
+++ configs/projects/puppet-agent.rb
@@ -12,9 +12,11 @@ project "puppet-agent" do |proj|
   settings[:puppet_runtime_basename] = "agent-runtime-#{agent_branch}-#{runtime_details['version']}.#{platform.name}"
 
   settings_uri = File.join(runtime_details['location'], "#{proj.settings[:puppet_runtime_basename]}.settings.yaml")
+
   sha1sum_uri = "#{settings_uri}.sha1"
-  metadata_uri = File.join(runtime_details['location'], "#{runtime_details['version']}.build_metadata.agent-runtime-#{agent_branch}.#{platform.name}.json")
-  proj.inherit_yaml_settings(settings_uri, sha1sum_uri, metadata_uri: metadata_uri)
+  proj.inherit_yaml_settings(settings_uri, sha1sum_uri)
+  # metadata_uri = File.join(runtime_details['location'], "#{runtime_details['version']}.build_metadata.agent-runtime-#{agent_branch}.#{platform.name}.json")
+  # proj.inherit_yaml_settings(settings_uri, sha1sum_uri, metadata_uri: metadata_uri)
 
   # (PA-678) pe-r10k versions prior to 2.5.0.0 ship gettext gems.
   # Since we also ship those gems as part of puppet-agent
